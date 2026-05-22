c12d81ade12f8d71af55eeec35c1e943e9f95e7c
Tawanda Kasunzuma <tskasunzuma@gmail.com>
2026-05-22 22:03:47 +0200

    Add Supabase configuration and initialization

---
+++ lib/core/constants/supabase_config.dart
@@ -0,0 +1,7 @@
+class SupabaseConfig {
+  static const supabaseUrl =
+      "https://kqgttuqwrenmrlrfioae.supabase.co";
+
+  static const supabaseAnonKey =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtxZ3R0dXF3cmVubXJscmZpb2FlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk0Njc0MzksImV4cCI6MjA5NTA0MzQzOX0.SQ0XNSzhTUwGStUFjo6-UVastasScH7XIq0C2lPejBo";
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+import 'core/constants/supabase_config.dart';
 import 'core/app_router.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: SupabaseConfig.supabaseUrl,
+    anonKey: SupabaseConfig.supabaseAnonKey,
+  );
+
+  runApp(const GospelApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class GospelApp extends StatelessWidget {
+  const GospelApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      title: "Gospel App",
       debugShowCheckedModeBanner: false,
-      routerConfig: AppRouter.router,
+      routerConfig: appRouter,
     );
   }
 }
